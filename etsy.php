51a72ba56d5bb854280558019aba4efee927c814
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2020-05-12 16:36:54 +0800

    Add WordPress hooks for admin settings page

--- etsy.php
+++ etsy.php
@@ -51,5 +51,45 @@ require dirname( __FILE__ ) . '/vendor/autoload.php';
 		}
 	);
 
+	add_action( 'init', __NAMESPACE__ . '\register_settings' );
+	add_action( 'admin_menu', __NAMESPACE__ . '\admin_menu' );
+	add_action( 'admin_print_scripts-settings_page_wpreact_page', __NAMESPACE__ . '\admin_print_scripts' );
 } )();
 
+/**
+ * Hook to provide admin menu and page.
+ */
+function admin_menu() {
+	add_options_page(
+		__( 'WP Etsy Listings', 'wpreact' ),
+		__( 'WP Etsy Listings', 'wpreact' ),
+		'manage_options',
+		'wpreact_page',
+		function() {
+			echo '<div id="wp-etsy-listings-container"></div>';
+		}
+	);
+}
+
+/**
+ * Hook to provide assets.
+ */
+function admin_print_scripts() {
+	$build = require __DIR__ . '/dist/admin.asset.php';
+	wp_enqueue_style( 'wp-components' );
+	wp_enqueue_script( 'wp-etsy-listings-admin', plugins_url( './dist/admin.js', __FILE__ ), $build['dependencies'], $build['version'], true );
+}
+
+/**
+ * Hook to provide settings.
+ */
+function register_settings() {
+	register_setting(
+		'codeinwp_settings',
+		'wp_etsy_listings_shop',
+		array(
+			'type'         => 'string',
+			'show_in_rest' => true,
+		)
+	);
+}
